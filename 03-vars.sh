44e91873dd32f240a5f6dc79defcf553ef4adb62
Jean TB <tanobian@gmail.com>
2023-10-18 22:33:15 -0700

    doing it

---
+++ 03-vars.sh
@@ -0,0 +1,14 @@
+# If we name some data it is called as variable
+
+# Syntaxe to decalre a variable
+# var_name-data
+
+a=10
+x=100
+y=true
+name=Devops
+
+
+# How to access those variables
+
+echo value of the a = $a
